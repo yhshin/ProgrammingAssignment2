3834db5ab8d7a901ef7de61563e41f34bb832389
Yun-Ha Shin <ryanyhs@gmail.com>
2015-02-14 22:30:00 -0800

    Implemented makeCacheMatrix() and cacheSolve() functions. Added a script to test the above functions too.

---
+++ test.cachematrix.R
@@ -0,0 +1,71 @@
+##
+## This is a sctipr to test makeCacheMatrix() and cacheSolve() functions.
+##
+source("cachematrix.R")
+
+testCacheMatrix <- function(n=100) {
+
+    # Generate nxn random numbers to be used as matrix elements
+    nelem <- n*n
+    #elems <- sample(1:nelem, replace=TRUE)
+    elems <- runif(nelem)
+    
+    # Create a test matrix
+    m1 <- matrix(elems, n,n)
+    
+    # Create a wrapper object
+    mc <- makeCacheMatrix(m1)
+    
+    # Solve the inverse matix
+    message("calculating inverse")
+    system.time(cacheSolve(mc))
+    
+    # Print them
+    message("printing part of matrix")
+    printMatrix(mc$get())
+
+    message("printing part inverse matrix")
+    printMatrix(mc$getInvMatrix())
+    
+    # Let's calculate it again
+    message("calculating inverse again")
+    system.time(cacheSolve(mc))
+    
+    #
+    # Do it again for new matrix
+    #
+    m2 <- matrix(runif(nelem), n,n)
+    
+    # Instead of creating a new cache, set new value to the cache
+    mc$set(m2)
+    
+    # Solve the inverse matix
+    message("calculating inverse of new cache value")
+    system.time(cacheSolve(mc))
+    
+    # Print them
+    message("printing part of matrix")
+    printMatrix(mc$get())
+    
+    message("printing part of inverse matrix")
+    printMatrix(mc$getInvMatrix())
+    
+    # Let's calculate it again
+    message("calculating inverse again")
+    system.time(cacheSolve(mc))
+}
+
+#
+# Print part of matrix
+#
+printMatrix <- function(m=matrix(), nhead=5) {
+    if (is.null(m)) {
+        warning("Null matrix is given")
+        return(NULL)
+    }
+    nrow <- nrow(m)
+    nshow <- min(nhead, nrow)
+    mpart <- head(m[,1:nshow], nshow)
+    print(mpart)
+    return (NULL)
+}
